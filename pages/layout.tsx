58242b766972a0b32cc25261c8bd4047347e98d6
nikl1425 <nmhjort7@gmail.com>
2022-12-26 10:36:39 +0100

    SideBar slide effect

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect, useState } from "react";
-import { Grid, GridItem, Box, Container, Flex, Slide } from '@chakra-ui/react';
+import { Grid, GridItem, Box, Container, Flex, Slide, Collapse } from '@chakra-ui/react';
 import NavBar from '../src/components/navBar/NavBar';
 import SideBar from "../src/components/sidebar/SideBar";
 import { Bool } from "reselect/es/types";
@@ -13,18 +13,6 @@ type Props = {
 const SIDEBAR_WIDTH: number = 15;
 
 
-const SwitchItem: FC<{ active: Boolean, width: number }> = ({ active, width }) => {
-    if (!active) {
-        return <Box transition="linear" transitionDuration='300ms'></Box>
-    } else {
-        return (
-            <Box transition="linear" transitionDuration='300ms'bg='white' w={`${width}%`}>
-                <SideBar title="sidebar" />
-            </Box>
-        )
-    }
-}
-
 const Layout: FC<Props> = ({ children }) => {
     const sideBarActive = useAppSelector((state) => state.AppManager.sideBarActive);
 
@@ -37,13 +25,19 @@ const Layout: FC<Props> = ({ children }) => {
                         <NavBar />
                     </Container>
                 </Box>
-                <Box h='100%' marginTop='10px' overflowY='auto' bg='white'>
-                    <Flex direction='row'>
-                            <SwitchItem width={SIDEBAR_WIDTH} active={sideBarActive} />
-                            <Box transition="linear" transitionDuration='300ms' w={sideBarActive ? `${100 - SIDEBAR_WIDTH}%` : '100%'}>
-                                {children}
+                <Box className="sideBar" h='100%' marginTop='10px' overflowY='auto' bg='white'>
+                    <Flex>
+                        
+                            <Box w={sideBarActive ? `${SIDEBAR_WIDTH}%` : '0%'} transitionDuration='1s'>
+                                <Collapse in={sideBarActive} animateOpacity>
+                                    <SideBar title="sidebar" />
+                                </Collapse>
                             </Box>
                         
+                            
+                            <Box w={sideBarActive ? `${100 - SIDEBAR_WIDTH}%` : '100%'} transitionDuration='1s'>
+                                {children}
+                            </Box>
                     </Flex>
                 </Box>
             </Flex>
