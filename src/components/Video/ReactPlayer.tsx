d2914218a6a854aaa5fd05fd7aad7c0d4a32f5f6
nikl1425 <nmhjort7@gmail.com>
2023-01-08 17:36:25 +0100

    fixed load and icon size

--- src/components/Video/ReactPlayer.tsx
+++ src/components/Video/ReactPlayer.tsx
@@ -1,19 +1,31 @@
-import ReactPlayer from "react-player";
+
 import React, { FC } from 'react';
 import { Box } from '@chakra-ui/react';
+import dynamic from 'next/dynamic';
+const ReactPlayer = dynamic(() => import('react-player'), { ssr: false })
+import styles from './ReactPlayer.module.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlayCircle } from "@fortawesome/free-regular-svg-icons";
 
 type VideoPlayerStyle = {
     color?: string,
     borderRadius?: string,
 }
 
+const PlayIcon: FC = () => (
+    <Box textColor={'green.300'}>
+        <FontAwesomeIcon size={'5x'} icon={faPlayCircle} />
+    </Box>
+);
+
 
 const CsReactPlayer: FC<{ link: string }> = ({ link }) => {
-    return (
-        <Box h={'full'}>
-            <ReactPlayer playing={true} height={'100%'}  width={'100%'} controls={true} url={'https://www.youtube.com/watch?v=ysz5S6PUM-U'} light={true} />
-        </Box>
 
+
+    return (
+        <Box position={'relative'} paddingTop={'56.25%'}>
+            <ReactPlayer playIcon={<PlayIcon />} className={styles.react_player} playing={true} height={'100%'} width={'100%'} controls={true} url={'https://www.youtube.com/watch?v=ysz5S6PUM-U'} light={true} />
+        </Box>
     )
 }
 
